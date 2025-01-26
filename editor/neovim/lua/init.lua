dc1cebc1d2262d6ddeea64a2e778b06e3af0293e
Hemanth Alluri <hdrive1999@gmail.com>
2025-01-26 00:45:39 -0800

    neovim: Disable node and python3 providers.

--- editor/neovim/lua/init.lua
+++ editor/neovim/lua/init.lua
@@ -27,6 +27,8 @@ end
 -- Disable the unnecessary Neovim providers.
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
 
 -- Use lazy.nvim for plugin management.
 --
