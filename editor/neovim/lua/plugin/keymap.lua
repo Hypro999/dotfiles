f67f73eeec3fe4b28193328f55e4959a8acc1db6
Hemanth Alluri <hdrive1999@gmail.com>
2025-02-17 22:32:12 -0800

    neovim/lua: Overhaul keymap.

--- editor/neovim/lua/plugin/keymap.lua
+++ editor/neovim/lua/plugin/keymap.lua
@@ -5,8 +5,8 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader><leader>", vim.cmd.noh)
 
 -- Close the quickfix and location lists quickly.
-vim.keymap.set("n", "<leader>cc", vim.cmd.cclose)
-vim.keymap.set("n", "<leader>lc", vim.cmd.lclose)
+vim.keymap.set("n", "<leader>qc", vim.cmd.cclose)
+vim.keymap.set("n", "<leader>ql", vim.cmd.lclose)
 
 -- Define unimpaired-style keybindings for navigating diagnostics.
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
@@ -14,29 +14,29 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<C-w>d", vim.diagnostic.open_float)
 
 -- Define LSP mappings.
-vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover)
-vim.keymap.set("n", "<leader>ld", function()
+vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>d", function()
         vim.lsp.buf.definition({ loclist = true })
     end)
-vim.keymap.set("n", "<leader>li", function()
+vim.keymap.set("n", "<leader>i", function()
         vim.lsp.buf.implementation({ loclist = true })
     end)
-vim.keymap.set("n", "<leader>lr", function()
+vim.keymap.set("n", "<leader>r", function()
         vim.lsp.buf.references(nil, ({ loclist = true }))
     end)
-vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 
 -- Define mappings to quickly open neovim config files.
-vim.keymap.set("n", "<leader>vc",
+vim.keymap.set("n", "<leader>1",
     function()
         vim.cmd.edit(vim.env.MYVIMRC)
     end)
-vim.keymap.set("n", "<leader>vk",
+vim.keymap.set("n", "<leader>2",
     function()
         vim.cmd.edit(vim.fn.stdpath("config") .. "/plugin/keymap.lua")
     end)
-vim.keymap.set("n", "<leader>vp",
+vim.keymap.set("n", "<leader>3",
     function()
         vim.cmd.edit(vim.fn.stdpath("config") .. "/lua/plugins.lua")
         vim.fn.search("require(\"lazy\").setup")
