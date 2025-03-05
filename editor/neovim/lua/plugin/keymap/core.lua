148531f04e7620a06664010cc0479e5fa2e99628
Hemanth Alluri <hdrive1999@gmail.com>
2025-03-04 21:23:04 -0800

    keymap/core.lua: Add keybinding to show current buffer abs path.

--- editor/neovim/lua/plugin/keymap/core.lua
+++ editor/neovim/lua/plugin/keymap/core.lua
@@ -8,3 +8,6 @@ vim.keymap.set("n", "<leader><leader>", vim.cmd.noh)
 vim.keymap.set("n", "<leader>qc", vim.cmd.cclose)
 vim.keymap.set("n", "<leader>ql", vim.cmd.lclose)
 
+-- Print absolute path of the current buffer
+vim.keymap.set("n", "<leader>fp", function() print(vim.api.nvim_buf_get_name(0)) end)
+
