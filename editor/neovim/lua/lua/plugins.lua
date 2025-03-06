b9d64a4bd53ba2214a177073a731228aa3083334
Hemanth Alluri <hdrive1999@gmail.com>
2025-03-05 19:40:45 -0800

    plugins.lua: Extend treesitter configuration.

--- editor/neovim/lua/lua/plugins.lua
+++ editor/neovim/lua/lua/plugins.lua
@@ -40,9 +40,23 @@ require("lazy").setup {
             "nvim-treesitter/nvim-treesitter",
             config = function()
                 require("nvim-treesitter.configs").setup {
+                    ensure_installed = {
+                        "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc",
+                        "json", "yaml", "xml",
+                        "python", "java", "go", "haskell"
+                    },
                     auto_install = true,
                     highlight = { enable = true },
-                    indent = { enable = true }
+                    indent = { enable = true },
+                    incremental_selection = {
+                        enable = true,
+                        keymaps = {
+                            init_selection = "<leader>s",
+                            node_incremental = "[n",
+                            scope_incremental = "[s",
+                            node_decremental = "]n"
+                        }
+                    }
                 }
             end
         },
