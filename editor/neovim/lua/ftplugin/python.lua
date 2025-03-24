339de4a7359cdadb8b56ff6d3d6152e6c65bdc1c
Hemanth Alluri <halluri@amazon.com>
2025-03-24 09:39:36 -0700

    neovim/lua: Minor keymap changes.

---
+++ editor/neovim/lua/ftplugin/python.lua
@@ -0,0 +1,1 @@
+require("lspconfig").anakin_language_server.setup {}
--- editor/neovim/lua/plugin/keymap/core.lua
+++ editor/neovim/lua/plugin/keymap/core.lua
@@ -8,6 +8,9 @@ vim.keymap.set("n", "<leader><leader>", vim.cmd.noh)
 vim.keymap.set("n", "<leader>qc", vim.cmd.cclose)
 vim.keymap.set("n", "<leader>ql", vim.cmd.lclose)
 
--- Print absolute path of the current buffer
+-- Close the current buffer.
+vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
+
+-- Print absolute path of the current buffer.
 vim.keymap.set("n", "<leader>fp", function() print(vim.api.nvim_buf_get_name(0)) end)
 
