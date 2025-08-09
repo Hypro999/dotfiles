c29adbf6cf25d8069011e161750a99d7f6d2abd5
Hemanth Alluri <hdrive1999@gmail.com>
2025-08-09 11:48:37 -0700

    Modify nvim keybinds.

--- editor/nvim/lua/keybinds.lua
+++ editor/nvim/lua/keybinds.lua
@@ -2,13 +2,13 @@
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Clear search highlighting.
-vim.keymap.set("n", "<leader>h", vim.cmd.noh)
+vim.keymap.set("n", "<leader><leader>", vim.cmd.noh)
 
 -- Close the current buffer.
 vim.keymap.set("n", "<leader>q", vim.cmd.bdelete)
 
 -- Print absolute path of the current buffer.
-vim.keymap.set("n", "<leader>fp", function() print(vim.api.nvim_buf_get_name(0)) end)
+vim.keymap.set("n", "<leader>?", function() print(vim.api.nvim_buf_get_name(0)) end)
 
 -- Bookmarks to important config files.
 vim.keymap.set("n", "<leader>1",
